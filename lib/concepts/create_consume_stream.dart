2943cb9c15ed294ce06c1d4740ed0c41584c31c3
Brett Maes <brett@solarisit.co.za>
2025-10-25 10:40:34 +0200

    2.1 Consume Stream

--- lib/concepts/create_consume_stream.dart
+++ lib/concepts/create_consume_stream.dart
@@ -1,10 +1,24 @@
-void main() {
+void main() async {
   // Initialise the stream
   var stream = countStream(10);
+
+  // Consume the stream
+  var total = await sumStream(stream);
+
+  // ignore: avoid_print
+  print(total);
 }
 
 Stream<int> countStream(int max) async* {
   for (int i = 0; i < max; i++) {
     yield i;
   }
+}
+
+Future<int> sumStream(Stream stream) async {
+  int sum = 0;
+  await for (int value in stream) {
+    sum += value;
+  }
+  return sum;
 }
