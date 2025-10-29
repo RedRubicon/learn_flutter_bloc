3d4363a4185f4fc381bf69734243ece329df35f0
Brett Maes <brett@solarisit.co.za>
2025-10-29 19:51:49 +0200

    4 - observing changes in cubit instance

--- lib/concepts/cubits.dart
+++ lib/concepts/cubits.dart
@@ -29,4 +29,11 @@ class CounterCubit extends Cubit<int> {
   CounterCubit() : super(0);
 
   void increment() => emit(state + 1);
+
+  @override
+  void onChange(Change<int> change) {
+    super.onChange(change);
+    // ignore: avoid_print
+    print(change);
+  }
 }
