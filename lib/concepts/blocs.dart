8f7ed31d0a27b4568fb891fc99b38268f550b20a
Brett Maes <brett@solarisit.co.za>
2025-11-02 16:26:36 +0200

    2 - Implement CounterBloc to handle CounterIncrementPressed events

--- lib/concepts/blocs.dart
+++ lib/concepts/blocs.dart
@@ -5,5 +5,9 @@ sealed class CounterEvent {}
 final class CounterIncrementPressed extends CounterEvent {}
 
 class CounterBloc extends Bloc<CounterEvent, int> {
-  CounterBloc() : super(0);
+  CounterBloc() : super(0) {
+    on<CounterIncrementPressed>((event, emit) {
+      emit(state + 1);
+    });
+  }
 }
